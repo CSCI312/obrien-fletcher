0fa0e4e7cfa9c02412d79ffac654d799ebc6e353
Fletcher O'Brien <fjobrien@wm.edu>
2025-03-25 13:21:59 -0400

    Add Data Structure

--- ppl/Assignment2/cdecl.c
+++ ppl/Assignment2/cdecl.c
@@ -1,1 +1,8 @@
+#define MAXTOKENLEN 64
+#define MAXTOKENS 100
 
+struct token { char type; char string[MAXTOKENLEN]; };
+/* holds tokens we read before reaching first identifier */
+struct token stack[MAXTOKENS];
+/* holds the token just read */
+struct token this; 
