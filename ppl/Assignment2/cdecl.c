f9b9ed6110504fa189cf7a6a7a19e31c294dd984
Fletcher O'Brien <fjobrien@wm.edu>
2025-03-26 09:50:06 -0400

    Modify Classify String

--- ppl/Assignment2/cdecl.c
+++ ppl/Assignment2/cdecl.c
@@ -10,12 +10,21 @@ struct token stack[MAXTOKENS];
 /* holds the token just read */
 struct token this;
 
+/*1.get current token
+2. set this.string equal to token
+3. classify it, set this.type
+4. if not identifier, push onto stack
+5. */
+
 /* Classify string looks at the current token and
-return a value of "type" "qualifier" or "indentifier"*/
+return a value of "type" "qualifier" or "indentifier"
+Need to look at this.string so we can set this.type*/
 char classify_string(void){
-    return this.type;
+    if (strcmp(this.string, "example")){
+        return 'q';
+    }
 }
 
 int gettoken(void) {
-    
+    this.string = stack[1].string;
 }
