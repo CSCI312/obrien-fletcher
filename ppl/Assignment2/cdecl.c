24b06c0666ec33f584513fabcd3f9eeedbace473
Fletcher O'Brien <fjobrien@wm.edu>
2025-03-26 09:59:46 -0400

    Fix Classify String

--- ppl/Assignment2/cdecl.c
+++ ppl/Assignment2/cdecl.c
@@ -20,9 +20,28 @@ struct token this;
 return a value of "type" "qualifier" or "indentifier"
 Need to look at this.string so we can set this.type*/
 char classify_string(void){
-    if (strcmp(this.string, "example")){
-        return 'q';
+    if (strcmp(this.string, "restrict") == 0 ||
+        strcmp(this.string, "volatile") == 0 ||
+        strcmp(this.string, "const") == 0){
+            this.type = 'q';
+            return 'q';
     }
+
+    if (strcmp(this.string, "void") == 0 ||
+        strcmp(this.string, "char") == 0 ||
+        strcmp(this.string, "short") == 0 ||
+        strcmp(this.string, "int") == 0 ||
+        strcmp(this.string, "long") == 0 ||
+        strcmp(this.string, "float") == 0 ||
+        strcmp(this.string, "double") == 0 ||
+        strcmp(this.string, "signed") == 0 ||
+        strcmp(this.string, "unsigned") == 0){
+            this.type = 't';
+            return 't';
+    }
+
+    this.type = 'i';
+    return 'i';
 }
 
 int gettoken(void) {
