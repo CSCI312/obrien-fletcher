d3b07ead3133fd0af1af3f00bdb88a910ae5f7a6
Fletcher O'Brien <fjobrien@wm.edu>
2025-03-26 10:49:35 -0400

    Test Output With Scan Symbol Too

--- ppl/Assignment2/cdecl.c
+++ ppl/Assignment2/cdecl.c
@@ -15,12 +15,6 @@ struct token stack[MAXTOKENS];
 /* holds the token just read */
 struct token this;
 
-/*1.get current token
-2. set this.string equal to token
-3. classify it, set this.type
-4. if not identifier, push onto stack
-5. */
-
 /* Classify string looks at the current token and
 return a value of "type" "qualifier" or "indentifier"
 Need to look at this.string so we can set this.type*/
@@ -52,13 +46,33 @@ char classify_string(void){
 int gettoken(void) {
     //read the next token into this.string
     //if it is alphanumeric, classify_string
+    if (scanf(" %99[^A-Za-z0-9 \t\n]", this.string) == 1){
+
+        if (strlen(this.string) >= 1){
+            this.type = *this.string; //this.type = the token itself
+            this.string[0] = '\0'; //terminate with a nul
+            return 0;
+        }
+    }
+
     if (scanf("%[A-Za-z0-9_]", this.string) == 1){
 
         if (strlen(this.string) == 1){
-            this.type = this.string;
-            this.string[0] = '\0';
+            this.type = *this.string; //this.type = the token itself
+            this.string[0] = '\0'; //terminate with a nul
         } else {
             classify_string();
         }
     }
+}
+
+/*1.get current token
+2. set this.string equal to token
+3. classify it, set this.type
+4. if not identifier, push onto stack
+5. */
+int main(void){
+    gettoken();
+    printf("%s\n", this.string);
+    printf("%c\n", this.type);
 }
