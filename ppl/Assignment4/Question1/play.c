7dd809ee9d1e16c7caec09eb3783e2e047838c32
root <root@ip-172-31-23-93.ec2.internal>
2025-04-09 13:25:51 +0000

    Complete Part Two

--- ppl/Assignment4/Question1/play.c
+++ ppl/Assignment4/Question1/play.c
@@ -5,9 +5,20 @@ void one(char ca[]){
         printf("ca[0]: %p\n", (void *)&ca[0]);
         printf("ca[1]: %p\n", (void *)&ca[1]);
 }
+void two(char *pa) {
+    printf("pa: %p\n", (void *)&pa);
+    printf("pa[0]: %p\n", (void *)&pa[0]);
+    printf("pa[1]: %p\n", (void *)&pa[1]);
+
+    pa++;
+    printf("++pa:%p\n", (void *)pa);
+}
+
 
 int main() {
         char ca[2] = {'a', 'b'};
         one(ca);
+        char *pa = "abc";
+        two((void *)pa);
         return 0;
 }
