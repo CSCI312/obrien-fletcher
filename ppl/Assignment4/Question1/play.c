16cf272bf79cf587bcddb56b66b55e0718fc3db9
root <root@ip-172-31-23-93.ec2.internal>
2025-04-09 13:34:17 +0000

    Complete Parts 3, 4, and 5

--- ppl/Assignment4/Question1/play.c
+++ ppl/Assignment4/Question1/play.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+char ga[] = "abcdefghijklmnopqrtsuvwxyz";
+
 void one(char ca[]){
         printf("ca: %p\n", (void *)&ca);
         printf("ca[0]: %p\n", (void *)&ca[0]);
@@ -20,5 +22,10 @@ int main() {
         one(ca);
         char *pa = "abc";
         two((void *)pa);
+	one(ga);
+	two(ga);
+	printf("ga: %p\n", (void *)&ga);
+        printf("ga[0]: %p\n", (void *)&ga[0]);
+        printf("ga[1]: %p\n", (void *)&ga[1]);
         return 0;
 }
