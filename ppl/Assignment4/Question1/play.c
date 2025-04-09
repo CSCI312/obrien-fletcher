e0ed4d6130d95b5e0926e1496f25d5bb9958c1f1
root <root@ip-172-31-23-93.ec2.internal>
2025-04-09 13:14:29 +0000

    Complete Part One

--- ppl/Assignment4/Question1/play.c
+++ ppl/Assignment4/Question1/play.c
@@ -1,1 +1,13 @@
+#include <stdio.h>
 
+void one(char ca[]){
+        printf("ca: %p\n", (void *)&ca);
+        printf("ca[0]: %p\n", (void *)&ca[0]);
+        printf("ca[1]: %p\n", (void *)&ca[1]);
+}
+
+int main() {
+        char ca[2] = {'a', 'b'};
+        one(ca);
+        return 0;
+}
