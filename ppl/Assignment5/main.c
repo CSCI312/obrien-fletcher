dfda3fd77db8d296123620b1528c6a1e7b447f79
Fletcher O'Brien <fjobrien@wm.edu>
2025-04-30 11:34:16 -0400

    Fix main.c

--- ppl/Assignment5/main.c
+++ ppl/Assignment5/main.c
@@ -4,31 +4,33 @@
 
 int main(int argc, char *argv[])
 {
+    if (argc != 2) {
+        fprintf(stderr, "Usage: %s <data_file>\n", argv[0]);
+        exit(1);
+    }
+
     char *fname = argv[1];
-    int number_of_examples = atoi(argv[2]);
 
     // Building
-    Data xcoords = new_Data(number_of_examples);
-    Data ycoords = new_Data(number_of_examples);
-    Data targets = new_Data(number_of_examples);
-    load_data(fname, xcoords, ycoords, targets);
-
-    Model model = new_Model();
-    initialize_model(model);
+    Data data = new_Data(fname);
+    Model model = new_Model(data);
 
     // Training
-    fit_model(model, xcoords, ycoords, targets, number_of_examples);
-
-    // Testing
-    // TODO
+    fit_model(model, data);
 
     // Scoring
     run_scoring_engine(model);
 
-    free(xcoords);
-    free(ycoords);
-    free(targets);
+    // Clean up
+    for (int i = 0; i < data->dims.num_examples; i++) {
+        free(data->elements[i]);
+    }
+    free(data->elements);
+    free(data->targets);
+    free(data);
+    free(model->weights);
     free(model);
 
     return 0;
 }
+
